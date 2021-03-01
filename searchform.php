f3b4f1429bf090c937d97cadb6b11bc484bdcbaa
vladimir <palkinw@mail.ru>
2021-03-01 21:47:32 +0300

    исправил форму

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url('/') ?>" >
-<input type="text" placeholder="Поиск" value="<?php echo get_search_query() ?>" name="s" id="s" />
-<button class="" type="submit" id="searchsubmit"></button>  
+<form class="search-form" role="search" method="get" id="searchform" action="<?php echo home_url('/') ?>" >
+<input  type="text" placeholder="Поиск" value="<?php echo get_search_query() ?>" name="s" id="s" />
+<button  type="submit" id="searchsubmit"></button>  
 </form>
