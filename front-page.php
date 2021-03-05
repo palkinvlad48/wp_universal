087559b254032dc02c5f65e1c930c999543c0b77
vladimir <palkinw@mail.ru>
2021-03-05 21:18:14 +0300

    6-й урок

--- front-page.php
+++ front-page.php
@@ -8,7 +8,8 @@
 
           $myposts = $posts = get_posts( [
 	          'numberposts' => 1,
-	          'category_name' => 'javascript, html, css, web-disign',
+            'offset' => 11,
+	          'category_name' => 'javascript',
             ]
           );
           // Есть ли посты
@@ -18,10 +19,10 @@
         ?>
         <!-- выводим записи -->
       
-        <img src="<?php the_post_thumbnail_url(); ?> class="post-thumb" alt="">
+        <img src="<?php the_post_thumbnail_url(); ?>" class="post-thumb" alt="">
         <?php $autor_id = get_the_author_meta('ID'); ?>
         <a href="<?php echo get_author_posts_url($autor_id); ?>" class="author">
-          <img src="<?php echo get_avatar_url($autor_id); ?>" class="avatar"alt="">
+          <img src="<?php echo get_avatar_url($autor_id); ?>" class="avatar" alt="">
           <div class="author-bio">
             <span class="author-name"><?php the_author(); ?></span>
             <span class="author-rank">Должность</span> 
@@ -33,15 +34,14 @@
           <?php the_category(); ?>
           <h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h2>
           <a href="<?php echo get_permalink(); ?>" class="more">Читать далее</a>
-          
         </div>
-        <?php
-            }
-          } else {
-    // Постов не найдено
+      <?php
           }
-          wp_reset_postdata(); // сбрасываем $post
-        ?>
+        } else {
+    // Постов не найдено
+        }
+        wp_reset_postdata(); // сбрасываем $post
+      ?>
       </div>
       <div class="right">
         <h3 class="recommend">Рекомендуем</h3>
@@ -51,9 +51,8 @@
 
           $myposts = $posts = get_posts( [
 	          'numberposts' => 4,
-            'offset' => 4,
+            'offset' => 12,
             'category-name' => 'javascript, html, css, web-disign',
-           // 'offset' => 5,
             ]
           );
           // Есть ли посты
@@ -68,9 +67,9 @@
             <a class="post-permalink" href="<?php echo get_the_permalink(); ?>
               <h4 class="post-title">
                 <?php 
-                  if (strlen($post->post_title) > 90)
+                  if (strlen($post->post_title) > 94)
                     // mb_strimwidth($post->title, 0, 64, ' ...');
-                    echo substr($post->post_title, 0, 90) . ' ...';
+                    echo substr($post->post_title, 0, 94) . ' ...';
                   else
                     echo $post->post_title; 
                 ?>
@@ -91,11 +90,12 @@
 </main>
 <div class="container">
   <ul class="article-list">
-        <?php
+  <!-- Выводим записи -->
+    <?php
           global $post;
 
           $myposts = $posts = get_posts( [
-	          'numberposts' => 4,
+	          'numberposts' => 7,
             //'offset' => 1,
             'category_name' => 'articles',
             ]
@@ -105,8 +105,7 @@
             foreach( $myposts as $post ) {
               setup_postdata( $post );
         ?>
-        <!-- выводим записи -->
-        
+        <!-- выводим записи -->      
           <li class="article-item">
             <a class="article-permalink" href="<?php echo get_the_permalink(); ?>">
              <h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h4>
@@ -121,4 +120,99 @@
         wp_reset_postdata(); // сбрасываем $post
       ?>
         </ul>
-</div>
+  <!-- ./article-list -->
+  <ul class="article-grid">
+  <?php 
+    global $post;
+
+    $query = new WP_Query( [
+      'post_per_page' => 7,
+    ]);
+
+    if ( $query->have_posts() ) {
+      // создаем счетчик постов
+      $cnt = 0;
+      while ( $query->have_posts() ) {
+        $query->the_post();
+        $cnt++;
+        switch ($cnt) {
+          case '1':
+      // выводим 1-й пост
+  ?>
+    <li class="article-grid-item article-grid-item-1">
+      <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+        <img class="article-grid-thumb" src="<?php echo get_the_post_thumbnail_url();?>"  alt="">
+        <span class="category-name"><?php 
+          $category = get_the_category(); echo $category[0]->name; ?>
+        </span>
+        <h4 class="article-grid-title"><?php echo get_the_title(); ?></h4>
+        <div class="article-grid-excerpt"><?php the_excerpt(); ?></div>
+        <div class="article-grid-info">
+          <div class="author">
+            <?php $autor_id = get_the_author_meta('ID'); ?>
+            <img src="<?php echo get_author_posts_url($autor_id); ?>" alt="" class="author-avatar">
+            <span class="author-name"><strong><?php the_author(); ?></strong>: <?php echo mb_strimwidth(the_author_meta('description'), 0, 30, '...'); ?></span>
+          </div>
+          <div class="comments">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/comment.png' ?>" alt="icon comment" class="icon comments-icon">
+            <span class="comments-counter"><?php comments_number('0', '1', '%'  )?></span>
+          </div>
+        </div>
+      </a>
+    </li>
+  <?php
+      break;
+      case '2':
+      // выводим 2-й пост
+  ?>
+    <li class="article-grid-item article-grid-item-2">
+      <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="" class="article-grid-thumb">
+      <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+        <span class="tag">
+        <?php $posttags = get_the_tags(); 
+          if ($posttags) {
+            echo $posttags[0]->name . ' ';
+          }
+        ?>
+        </span>
+        <span class="category-name"><?php 
+          $category = get_the_category(); echo $category[0]->name; ?>
+        </span>
+        <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
+      </a>
+    </li>
+<?php
+      break;
+      case '3':
+      // выводим 3-й пост
+?>
+    <li class="article-grid-item article-grid-item-3">
+      <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+      <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="" class="article-grid-thumb">
+      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
+    </a>
+  </li>
+<?php 
+    break;
+    default:
+        // выводим остальные посты
+?>
+  <li class="article-grid-item article-grid-item-default">
+    <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
+      <p class="article-grid-excerpt"><?php echo mb_strimwidth(get_the_excerpt(), 0, 76, '...'); ?></p>
+      <span class="article-date"><?php the_time( 'j F' )?></span> 
+      <!-- если the_date, то дата выводится только в 1-м посте  -->
+    </a>
+  </li>
+<?php 
+      break;
+    }    
+  }
+} else {
+      // Нет постов
+}
+    wp_reset_postdata();
+?>    
+</ul>
+<!-- /.article-grid -->
--- functions.php
+++ functions.php
@@ -34,7 +34,6 @@ add_action( 'after_setup_theme', 'universal_theme_setup');
 
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css' );
   wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', time() );
 	wp_enqueue_style( 'Roboto-Slab', 'https://fonts/googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
 
@@ -47,9 +46,14 @@ add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
 function delete_intermediate_image_sizes( $sizes ) {
 	// размеры, которые удаляются
 	return array_diff( $sizes, [
-		'meium_large',
+		'medium_large',
 		'large',
 		'1536X1536',
 		'2048x2048',
 	]);
+}
+
+if ( function_exists( 'add_image_size') ) {
+	//add_image_size( 'front-page-thumb', 65, 999 ); // без ограничения по высоте
+	add_image_size( 'homepage-thumb', 65, 65, true ); // кадрирование картинки
 }
